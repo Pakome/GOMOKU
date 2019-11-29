3460f711bf0b639013257201ad6abd4534932810
Pakome <maxime.lardier@epitech.eu>
2019-11-29 23:53:04 +0100

    test

--- compile.py
+++ compile.py
@@ -2,4 +2,4 @@ import os
 
 os.system("pip install pyinstaller")
 os.system("pyinstaller main.py brain_commands.py --name pbrain-STG-LardHon.exe --onefile")
-os.system('copy .\\dist\\pbrain-STG-LardHon.exe .')
+os.system('mv ./dist/pbrain-STG-LardHon.exe .')
